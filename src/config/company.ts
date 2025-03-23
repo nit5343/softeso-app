ed2064661746bf30e9f1b18414d15058fdf307b7
Nitish Kumar <nit5343@hotmail.com>
2025-03-23 16:24:12 +0530

    Update favicon & company details

--- src/config/company.ts
+++ src/config/company.ts
@@ -2,8 +2,8 @@ export const COMPANY = {
   legalName: 'Softeso Global (OPC) Private Limited',
   shortName: 'Softeso',
   contact: {
-    email: 'support@softeso.com',
-    phone: '7863962312',
+    email: 'softesoglobal@gmail.com',
+    phone: '+91-78639-62312',
     address: 'Wing B, T G Ascent, Off Hosa Road, Bangalore, Karnataka, India, 560099'
   },
   businessHours: {
